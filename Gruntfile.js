4b3232e0a2fc59ac239b763a6e89d2e3a5738e92
Isabel Queiroz <belqrdev@outlook.com>
2024-01-26 10:19:05 -0300

    fix: bug grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -16,7 +16,74 @@ module.exports = function(grunt) {
                     'dist/styles/index.min.css': 'src/styles/index.less'
                 }
             }
+        },replace: {
+            dev: {
+                options: {
+                    patterns: [
+                        {
+                            match: 'CSS_ADDRESS',
+                            replacement: './styles/index.css'
+                        },
+                        {
+                            match: 'JS_ADDRESS',
+                            replacement: '../src/scripts/index.js'
+                        }
+                    ]
+                },
+                files: [
+                    {
+                        expand: true,
+                        flatten: true,
+                        src: ['src/index.html'],
+                        dest: 'dev/'
+                    }
+                ]
+            },
+            dist: {
+                options: {
+                    patterns: [
+                        {
+                            match: 'CSS_ADDRESS',
+                            replacement: './styles/index.min.css'
+                        },
+                        {
+                            match: 'JS_ADDRESS',
+                            replacement: './scripts/index.min.js'
+                        }
+                    ]
+                },
+                files: [
+                    {
+                        expand: true,
+                        flatten: true,
+                        src: ['prebuild/index.html'],
+                        dest: 'dist/'
+                    }
+                ]
+            }
         },
+        htmlmin: {
+            dist: {
+                options: {
+                    removeComments: true,
+                    collapseWhitespace: true,
+                },
+                files: {
+                    'prebuild/index.html': 'src/index.html'
+                }
+            }
+        },        
+        watch: {
+            less: {
+                files: ['src/styles/**/*.less'],
+                tasks: ['less:development']
+            },
+            html: {
+                files: ['src/index.html'],
+                tasks: ['replace:dev']
+            }
+        },
+        clean: ['prebuild'],
         uglify: {
             target: {
                 files: {
@@ -26,10 +93,14 @@ module.exports = function(grunt) {
         }
     })
 
-
+    grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-contrib-less');
     grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-htmlmin');
+    grunt.loadNpmTasks('grunt-replace');
 
-    grunt.registerTask('default', ['less', 'uglify']);
+    grunt.registerTask('default', ['watch']);
+    grunt.registerTask('build', ['less:production', 'htmlmin:dist', 'replace:dist', 'clean', 'uglify']);
 
 }
